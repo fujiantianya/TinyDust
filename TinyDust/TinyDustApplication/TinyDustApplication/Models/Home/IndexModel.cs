52dbc5ced977f75290cc918cea7acda7d0286df1
fujiantianya <fujiantianya@163.com>
2018-03-14 23:06:02 +0800

    Create repository

---
+++ TinyDust/TinyDustApplication/TinyDustApplication/Models/Home/IndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TinyDustApplication.Models.Home
+{
+    public class IndexModel
+    {
+        /// <summary>
+        /// The title.
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
